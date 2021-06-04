423ef0f04d56559363d781804ce915814f39f270
Yoonsik Jung <vivafrog@gmail.com>
2021-06-04 20:12:17 +0900

    6.6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -43,3 +43,21 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  res.render('upload', { pageTitle: 'Upload Video' });
+};
+export const postUpload = (req, res) => {
+  console.log(req.body);
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 5,
+    comments: 2,
+    createdAt: 'just now',
+    views: 59,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  res.redirect('/');
+};
