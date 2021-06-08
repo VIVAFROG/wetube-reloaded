2a2c286e82e7e92fdc00ab93a6dfe5de7518e2b0
Yoonsik Jung <vivafrog@gmail.com>
2021-06-08 23:37:23 +0900

    6.26

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 
+// export const formatHashtags = (hashtags) => hashtags
+//  .split(',')
+//  .map((word) => (word.startsWith('#') ? word : `#${word}`));
+
 const videoSchema = new mongoose.Schema({
   title: {
     type: String, required: true, trim: true, maxLength: 80,
@@ -15,11 +19,9 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
-videoSchema.pre('save', async function () {
-  this.hashtags = this.hashtags[0]
-    .split(',')
-    .map((word) => (word.startsWith('#') ? word : `#${word}`));
-});
+videoSchema.static('formatHashtags', (hashtags) => hashtags
+  .split(',')
+  .map((word) => (word.startsWith('#') ? word : `#${word}`)));
 
 const Video = mongoose.model('Video', videoSchema);
 export default Video;
