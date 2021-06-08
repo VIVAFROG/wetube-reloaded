0003a10d24f5d085f313d5710a462e516ee76fa4
Yoonsik Jung <vivafrog@gmail.com>
2021-06-08 21:18:04 +0900

    6.21

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,14 +2,25 @@ import Video from '../models/Video';
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
-  console.log(videos);
   return res.render('home', { pageTitle: 'Home', videos });
 };
-export const watch = (req, res) => {
-  res.render('watch', { pageTitle: 'Watchching ' });
+
+export const watch = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found.' });
+  }
+  return res.render('watch', { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
-  res.render('edit', { pageTitle: 'Editing: ' });
+
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render('404', { pageTitle: 'Video not found.' });
+  }
+  return res.render('edit', { pageTitle: `Edit ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
