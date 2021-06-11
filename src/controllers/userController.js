ed498d118fc833144cbb175b463b6db4c812fca3
Yoonsik Jung <vivafrog@gmail.com>
2021-06-12 04:46:48 +0900

    7.8

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import bcrpyt from 'bcrypt';
 import User from '../models/User';
 
 export const getJoin = (req, res) => res.render('Join', { pageTitle: 'Join' });
@@ -38,11 +39,25 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) => res.render('login', { pageTitle: 'Login' });
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const exists = await User.exists({ username });
-  if (!exists) {
-    return res.status(400).render('login', { pageTitle: 'Login', errorMessage: 'An account with this username does not exists.' });
+  const pageTitle = 'Login';
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).render('login', {
+      pageTitle,
+      errorMessage: 'An account with this username does not exists.',
+    });
   }
-  res.end();
+
+  const ok = await bcrpyt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render('login', {
+      pageTitle,
+      errorMessage: 'Wrong password',
+    });
+  }
+
+  return res.redirect('/');
 };
 
 export const edit = (req, res) => res.send('Edit User');
