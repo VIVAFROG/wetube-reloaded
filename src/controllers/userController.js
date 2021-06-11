ba9247b929d5455ddaed83040b4f94710bbbcd58
Yoonsik Jung <vivafrog@gmail.com>
2021-06-12 04:05:15 +0900

    7.4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,22 @@ import User from '../models/User';
 export const getJoin = (req, res) => res.render('Join', { pageTitle: 'Join' });
 export const postJoin = async (req, res) => {
   const {
-    name, username, email, password, location,
+    name, username, email, password, password2, location,
   } = req.body;
+  const pageTitle = 'Join';
+
+  if (password !== password2) {
+    return res.status(400).render('join', {
+      pageTitle,
+      errorMessage: 'Password confirmation does not match.',
+    });
+  }
+
+  const usernameExists = await User.exists({ $or: [{ username }, { email }] });
+  if (usernameExists) {
+    return res.status(400).render('join', { pageTitle, errorMessage: 'this username/email is already taken.' });
+  }
+
   await User.create({
     name,
     username,
