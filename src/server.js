7f40e9e0a0d9a7540d76179a7ad895a8fc14a1a5
Yoonsik Jung <vivafrog@gmail.com>
2021-06-12 05:06:58 +0900

    7.10

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from 'express-session';
 import rootRouter from './routers/rootRouter';
 import videoRouter from './routers/videoRouter';
 import userRouter from './routers/userRouter';
+import { localsMiddleware } from './middlewares';
 
 const app = express();
 const logger = morgan('dev');
@@ -20,16 +21,14 @@ app.use(session({
 }));
 
 app.use((req, res, next) => {
+  console.log(res);
   req.sessionStore.all((error, sessions) => {
     console.log(sessions);
     next();
   });
 });
 
-app.get('/add-one', (req, res, next) => {
-  req.session.potato += 1;
-  return res.send(`${req.session.id}\n${req.session.potato}`);
-});
+app.use(localsMiddleware);
 
 app.use('/', rootRouter);
 app.use('/videos', videoRouter);
