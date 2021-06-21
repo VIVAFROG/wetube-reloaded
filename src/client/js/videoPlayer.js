89c28626e2e4de566e57324d36ca8fe24094f3b6
Yoonsik Jung <vivafrog@gmail.com>
2021-06-21 20:50:05 +0900

    11.7

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
-const time = document.getElementById('time');
+const currentTime = document.getElementById('currentTime');
+const totalTime = document.getElementById('totalTime');
 const volumeRange = document.getElementById('volume');
+const timeline = document.getElementById('timeline');
+const fullScreenBtn = document.getElementById('fullScreen');
+const videoContainer = document.getElementById('videoContainer');
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -38,6 +42,38 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
+
+const handleLoadedMetaData = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+  const { target: { value } } = event;
+  video.currentTime = value;
+};
+
+const handleFullscreen = () => {
+  const fullScreen = document.fullscreenElement;
+  if (fullScreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = 'Enter Full Screen';
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerHTML = 'Exit Full Screen';
+  }
+};
+
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMuteClick);
 volumeRange.addEventListener('input', handleVolumeChange);
+video.addEventListener('loadedmetadata', handleLoadedMetaData);
+video.addEventListener('timeupdate', handleTimeUpdate);
+timeline.addEventListener('input', handleTimelineChange);
+fullScreenBtn.addEventListener('click', handleFullscreen);
