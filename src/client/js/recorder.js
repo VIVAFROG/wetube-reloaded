61d11e3fac1bfaab1471b7bb47f9850dc8fc3d44
Yoonsik Jung <vivafrog@gmail.com>
2021-06-23 22:47:12 +0900

    13.4

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,52 @@
 const startBtn = document.getElementById('startBtn');
 const video = document.getElementById('preview');
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement('a');
+  a.href = videoFile;
+  a.download = 'MyRecording.webm';
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = 'Download Recording';
+  startBtn.removeEventListener('click', handleStop);
+  startBtn.addEventListener('click', handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = 'Stop Recording';
+  startBtn.removeEventListener('click', handleStart);
+  startBtn.addEventListener('click', handleStop);
+  recorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
+
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    console.log(videoFile);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream;
-  await video.play();
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener('click', handleStart);
