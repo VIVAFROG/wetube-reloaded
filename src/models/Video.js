df846d1a4ff0d34ac8d106308265d5c14621bc7f
Yoonsik Jung <vivafrog@gmail.com>
2021-07-03 01:36:03 +0900

    thumbnail edit

--- src/models/Video.js
+++ src/models/Video.js
@@ -9,6 +9,7 @@ const videoSchema = new mongoose.Schema({
     type: String, required: true, trim: true, maxLength: 80,
   },
   fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
   description: {
     type: String, required: true, trim: true, minLength: 2,
   },
